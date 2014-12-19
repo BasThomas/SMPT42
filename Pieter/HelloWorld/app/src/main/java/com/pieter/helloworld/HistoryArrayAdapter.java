b0ae035b9724390313c1c2e0b0fdf3e2f54c033d
pieter kegel <pieterkegel@gmail.com>
2014-12-19 11:52:33 +0100

    History listview

---
+++ Pieter/HelloWorld/app/src/main/java/com/pieter/helloworld/HistoryArrayAdapter.java
@@ -0,0 +1,40 @@
+package com.pieter.helloworld;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Pieter on 19-12-2014.
+ */
+public class HistoryArrayAdapter extends ArrayAdapter<HistoryItemRow>
+{
+    private final Context context;
+    private final ArrayList<HistoryItemRow> itemsArrayList;
+
+    public HistoryArrayAdapter(Context context, ArrayList<HistoryItemRow> itemsArrayList)
+    {
+        super(context, R.layout.scan_item, itemsArrayList);
+        this.context = context;
+        this.itemsArrayList = itemsArrayList;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent)
+    {
+        LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View rowView = inflater.inflate(R.layout.scan_item, parent, false);
+
+        TextView tbTitle = (TextView)rowView.findViewById(R.id.tbTitle);
+        TextView tbCompany = (TextView)rowView.findViewById(R.id.tbCompany);
+        tbTitle.setText(itemsArrayList.get(position).Title);
+        tbCompany.setText(itemsArrayList.get(position).Company);
+
+        return rowView;
+    }
+}
---
+++ Pieter/HelloWorld/app/src/main/java/com/pieter/helloworld/HistoryItemRow.java
@@ -0,0 +1,19 @@
+package com.pieter.helloworld;
+
+/**
+ * Created by Pieter on 19-12-2014.
+ */
+public class HistoryItemRow
+{
+    //TODO: use get/set
+    public int itemId;
+    public String Title;
+    public String Company;
+
+    public HistoryItemRow(int itemId, String Title, String Company)
+    {
+        this.itemId = itemId;
+        this.Title = Title;
+        this.Company = Company;
+    }
+}
