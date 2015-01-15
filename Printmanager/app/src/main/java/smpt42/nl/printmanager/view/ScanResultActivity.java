e8f423dfffee5e8e1bed0390b9d65fef51896c60
mrbama1010 <markvandenhurk@gmail.com>
2015-01-15 20:33:32 +0100

    Starred added to detailed scan v1.0

--- Printmanager/app/src/main/java/smpt42/nl/printmanager/view/ScanResultActivity.java
+++ Printmanager/app/src/main/java/smpt42/nl/printmanager/view/ScanResultActivity.java
@@ -1,7 +1,9 @@
 package smpt42.nl.printmanager.view;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -12,8 +14,14 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
 import java.util.concurrent.ExecutionException;
 
 import smpt42.nl.printmanager.R;
@@ -30,12 +38,33 @@ public class ScanResultActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_result);
 
+
+
         final ImageButton ImageButtonBack = (ImageButton) findViewById(R.id.backImageButton);
+        final ImageView imageViewStar = (ImageView) findViewById(R.id.ImageViewStar);
+
+
 
         ImageButtonBack.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent intent = new Intent(ScanResultActivity.this, OverviewActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        imageViewStar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if(isStarred(scan.getName()))
+                {
+                    imageViewStar.setImageResource(R.drawable.details_star);
+                    removeStarred(scan.getName());
+                }
+                else
+                {
+                    imageViewStar.setImageResource(R.drawable.details_starred);
+                    setStarred(scan.getName());
+                }
+
             }
         });
 
@@ -50,6 +79,48 @@ public class ScanResultActivity extends Activity {
             e.printStackTrace();
         }
         updateLabels();
+
+
+    }
+
+    private boolean isStarred(String name)
+    {
+        SharedPreferences sharedPref = getSharedPreferences("printmanager_starred_scans", Context.MODE_PRIVATE);
+        Set<String> Set = new HashSet<>();
+        Set = sharedPref.getStringSet("starred_scans", null);
+
+        for (String temp : Set)
+        {
+            if(temp.equals(name))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void setStarred(String name)
+    {
+        SharedPreferences sharedPref = getSharedPreferences("printmanager_starred_scans", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        Set<String> set = new HashSet<>();
+        set = sharedPref.getStringSet("starred_scans", null);
+        set.add(name);
+        editor.putStringSet("starred_scans", set);
+        editor.commit();
+    }
+
+    private void removeStarred(String name)
+    {
+        SharedPreferences sharedPref = getSharedPreferences("printmanager_starred_scans", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        Set<String> set = new HashSet<>();
+        set = sharedPref.getStringSet("starred_scans", null);
+
+        set.remove(name);
+
+        editor.putStringSet("starred_scans", set);
+        editor.commit();
     }
 
     private void updateLabels(){
